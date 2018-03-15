fa514a5b59fbe2b71a9f46bae9dea6890f17294d
屈文斌 <twfx7758@hotmail.com>
2018-03-16 07:49:18 +0800

    提交

--- src/main/java/com/quwb/web/controller/OrderController.java
+++ src/main/java/com/quwb/web/controller/OrderController.java
@@ -1,7 +1,6 @@
 package com.quwb.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
